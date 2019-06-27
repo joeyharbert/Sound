5c00c9018466680d370307b847986cabb719d857
joeyharbert <joey.harbert@gmail.com>
2019-06-27 18:35:10 -0500

    Pulled HTML creation outside of getData to ato allow for better implementation of dropdown

--- Sound/wwwroot/site.js
+++ Sound/wwwroot/site.js
@@ -17,36 +17,8 @@ function getData() {
         var type = [];
 
       $(tBody).empty();
-        var innerHTML = "";
-        var i = 0;
-        $.each(data, function(key, item) {
-            const rowCount = 6;
-            const openRow = "<tr>"
-            const closeRow = "</tr>"
-            const openCell = "<td class=\"sound " + item.type + "\">"
-            const closeCell= "</td>"
-            const button = "<button onclick=\"playSound('" + item.url + "')\">" + item.title + "</button>";
-            var line = ""
-            // if i is divisible by the number of cells in a row it is the start of a row
-            if (i%rowCount === 0) {
-                line += openRow;
-            }
+        var innerHTML = tableHTML(data, type);
 
-            //add button
-            line = openCell + button + closeCell;
-
-            // if i + 1 is divisible by the number of cells in a row it is the end of a row
-            if ((i + 1) % rowCount === 0) {
-                line += closeRow
-            }
-            innerHTML += line;       
-            i++;
-
-            //add type to type array
-            if (!(type.includes(item.type))) {
-                type.push(item.type);
-            }
-        });
         tBody.innerHTML = innerHTML;
 
         var dropdownInnerHTML = dropdown.innerHTML;
@@ -62,6 +34,41 @@ function getData() {
   });
 }
 
+function tableHTML(data, type) {
+    var innerHTML = "";
+    var i = 0;
+    $.each(data, function(key, item) {
+        const rowCount = 6;
+        const openRow = "<tr>"
+        const closeRow = "</tr>"
+        const openCell = "<td class=\"sound " + item.type + "\">"
+        const closeCell= "</td>"
+        const button = "<button onclick=\"playSound('" + item.url + "')\">" + item.title + "</button>";
+        var line = ""
+        // if i is divisible by the number of cells in a row it is the start of a row
+        if (i%rowCount === 0) {
+            line += openRow;
+        }
+
+        //add button
+        line = openCell + button + closeCell;
+
+        // if i + 1 is divisible by the number of cells in a row it is the end of a row
+        if ((i + 1) % rowCount === 0) {
+            line += closeRow
+        }
+        innerHTML += line;       
+        i++;
+
+        //add type to type array
+        if (!(type.includes(item.type))) {
+            type.push(item.type);
+        }
+    });
+
+    return innerHTML;
+}
+
 function playSound(url) {
     console.log(url);
     var audio = new Audio(url);
@@ -71,14 +78,24 @@ function playSound(url) {
 function changeView() {
     const dropdown = document.getElementById("typedropdown");
     const value = dropdown.value;
-    const sounds = document.getElementsByClassName("sound");
+    /*
+    const soundElements = document.getElementsByClassName("sound");
 
-    for (var i = 0; i < sounds.length; i++) {
-        if (value == "All" || sounds[i].className.substring(6) == value) {
-            sounds[i].style.display = "";
+    for (var i = 0; i < soundElements.length; i++) {
+        if (value == "All" || soundElements[i].className.substring(6) == value) {
+            soundElements[i].style.display = "";
         } else {
-            sounds[i].style.display = "none";
+            soundElements[i].style.display = "none";
         }
+    } 
+    */
+
+    const tBody = document.getElementById("sounds");
+    var newSounds = sounds;
+
+    if (value !== "All") {
+        newSounds = sounds.filter(sound => sound.type == value);
     }
 
+    tBody.innerHTML = tableHTML(newSounds, []);
 }
